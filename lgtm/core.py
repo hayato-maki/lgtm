cc26f08c7ec3edcb1df3f3c07f1254b365c7074d
hayato-maki <hamaki631@gmail.com>
2020-09-05 21:54:51 +0900

    command args

--- lgtm/core.py
+++ lgtm/core.py
@@ -1,9 +1,10 @@
 import click
 
-
+@click.option('--message', '-m', default='LGTM', show_default=True, help='画像に乗せる文字')
+@click.argument('keyword')
 @click.command()
-def cli():
-    lgtm()
+def cli(keyword, message):
+    lgtm(keyword, message)
     click.echo('lgtm')
 
 def lgtm():
